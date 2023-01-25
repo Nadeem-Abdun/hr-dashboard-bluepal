42ea150654abd462ed118d5bb85c0cd43e1d2b9d
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2023-01-25 12:27:43 +0530

    Initial commit-1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Content from './Components/Content';
 function App() {
   return (
     <>
-      <Box display='block' height='100vh' width='100vw'>
-        <Box height='100%' width='100%' sx={{ display: { sm: 'none', md: 'block' } }}>
-          <Appbars />
-          <Content />
-        </Box>
-        <Box height='100%' width='100%' sx={{ display: { sm: 'block', md: 'none' } }}>
-          <Typography component='h2' variant='h2'>🚧⚠️This App is not optimized for mobile view, Please switch to a desktop view.⚠️🚧</Typography>
-        </Box>
+      <Box height='100vh' width='100vw' sx={{ display: { xs: 'none', lg: 'block' } }}>
+        <Appbars />
+        <Content />
+      </Box>
+      <Box height='100vh' width='100vw' sx={{ display: { xs: 'block', lg: 'none' } }}>
+        <Typography component='h2' variant='h2'>🚧⚠️This App is not optimized for mobile view, Please switch to a desktop view.⚠️🚧</Typography>
       </Box>
     </>
   );
